a7e957606f23ca5a05c95d446c4783b10ca8ff70
mllsumulong <mllsumulong@gmail.com>
2019-05-02 20:38:10 -0700

    initial commit for homework
    Name    :   Maricel Louise Sumulong
    Date    :   May 02, 2019 PDT
    E-mail  :   mllsumulong@gmail.com

---
+++ public/javascripts/app.js
@@ -0,0 +1,332 @@
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : 
+ #  AUTHOR        : 
+ #  DATE          : 
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : 
+ #  PARAMETERS    : 
+ #
+ #######################################################################
+*/
+
+//GLOBAL VARIABLES
+var did // to be used for deleting a saved article
+var fromDel = false
+var aid // to be used in saving comments for the article
+
+$(document).on("click","button",function() {
+
+	switch($(this).text().toLowerCase()) {
+		case "save article":
+			var id = $(this).attr("id")
+			var isSaved = checkArticle(id)
+
+			if (isSaved) {
+				alertMsg("Article was already in the database!","prompt")
+			} else {
+				saveArticle(id)
+			  }
+		break;
+		case "delete from saved":
+			var art = $(this)[0].parentElement.parentElement.previousElementSibling.title
+			did = $(this).attr("id").split("-")[1]
+			alertMsg("Are you sure you want to delete the article - <b>"+art+"</b> from <b>Saved Articles</b>?","confirm")
+		break;
+		case "yes":
+			deleteSavedArticle(did)
+		break;
+		case "no":
+			did = ""
+		break;
+		case "ok":
+			if (fromDel == true) {
+				fromDel = false
+				window.location.href = "/fetchSavedArticles"
+			} 
+		break;
+		case "article notes":
+			var id = $(this).attr("id").split("-")[1]
+			var art = $(this)[0].parentElement.parentElement.previousElementSibling.title
+			showArticles(id,art)
+		break;
+		case "close":
+			aid = ""
+		break;
+		case "save":
+			saveComments()
+		break;
+	}
+
+})
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : alertMsg
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 01, 2019 PDT
+ #  MODIFIED BY   : Maricel Louise Sumulong
+ #  REVISION DATE : May 02, 2019 PDT
+ #  REVISION #    : 1
+ #  DESCRIPTION   : alerts error message
+ #  PARAMETERS    : message, prompt type
+ #
+ #######################################################################
+*/
+
+function alertMsg(msg,conf) {
+
+  $("#alertModal .modal-body").empty().append(msg)
+  $("#alertModal").modal("show")
+
+  switch (conf) {
+  	case "confirm":
+  		$("#modalAlertBtn").hide();
+  		$("#yesAlertBtn, #noAlertBtn").show();
+  	break; 
+  	default:
+  		$("#modalAlertBtn").show();
+  		$("#yesAlertBtn, #noAlertBtn").hide();
+  	break;
+  }
+
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : saveArticle
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 02, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : save selected articles in the database
+ #  PARAMETERS    : article id
+ #
+ #######################################################################
+*/
+
+function saveArticle(id) {
+
+	$.ajax({
+	    url: '/save/' + id,
+	    method: 'PUT',
+	    async: false,
+	}).done(function(r){
+
+			if (r == "1") {
+				alertMsg("Article Saved!","prompt")
+			} else {
+				alertMsg("Error In Code!","prompt")	
+			  }
+
+
+	   });
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : checkArticle
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 01, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : checks if selected article is already in the database
+ #  PARAMETERS    : article id
+ #
+ #######################################################################
+*/
+
+function checkArticle(id) {
+
+	var ret 
+
+	$.ajax({
+	    url: '/check/' + id,
+	    method: 'GET',
+	    async: false,
+	}).done(function(r){
+
+			ret = r
+	   
+	   });
+
+	return ret
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : deleteSavedArticle
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 02, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : delete saved article
+ #  PARAMETERS    : article id
+ #
+ #######################################################################
+*/
+
+function deleteSavedArticle(id) {
+
+	$.ajax({
+	    url: '/delete/' + id,
+	    method: 'DELETE',
+	    async: false,
+	}).done(function(r){
+
+			did = ""
+
+			if (r == "1") {
+				fromDel = true
+				alertMsg("Article Deleted!","prompt")
+			} else {
+				alertMsg("Error In Code!","prompt")	
+			  }
+
+
+	   });
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : showArticles
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 02, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : show comments for articles
+ #  PARAMETERS    : article id, article name
+ #
+ #######################################################################
+*/
+
+function showArticles(id, art) {
+
+	aid = id
+	$.ajax({
+	    url: '/fetchComments/' + aid,
+	    method: 'GET',
+	    async: false,
+	}).done(function(r){
+
+			if ($("#articleModal").is(":visible")) {
+				var ar = $("#article-"+id)[0].parentElement.parentElement.previousElementSibling.title
+				$("#artTitle").empty().append("ARTICLE: <b>"+ar+"</b>")
+
+				$("#commentsDiv").empty()
+				if (r.length == 0) {
+					$("#commentsDiv").empty().append("<p>No comments for the article yet</p>")
+				} else {
+					for (var t = 0; t < r.length; t++) {
+						var x = $("<p>").text(r[t].comments)
+						var i = $("<i>").attr("class","fas fa-trash-alt")
+						i.attr("id",r[t]._id)
+						i.attr("onclick","deleteComment('"+r[t]._id+"')")
+						x.append("&nbsp;")
+						x.append(i)
+						$("#commentsDiv").append(x)
+						
+					}
+				  }
+
+			} else {
+				if (r == null) {
+					$("#commentsDiv").empty().append("<p>No comments for the article yet</p>")
+				} else if (r.length == 0) {
+					$("#commentsDiv").empty().append("<p>No comments for the article yet</p>")
+				} else {
+					$("#commentsDiv").empty()
+					console.log(r)
+					for (var t = 0; t < r.length; t++) {
+						var x = $("<p>").text(r[t].comments)
+						var i = $("<i>").attr("class","fas fa-trash-alt")
+						i.attr("id",r[t]._id)
+						i.attr("onclick","deleteComment('"+r[t]._id+"')")
+						x.append("&nbsp;")
+						x.append(i)
+						$("#commentsDiv").append(x)
+						
+					}
+				  }
+				$("#artTitle").empty().append("ARTICLE: <b>"+art+"</b>")
+				$("#articleModal").modal("show")
+			  }
+
+
+	   });
+ 
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : saveComments
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 02, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : save comments for the articles
+ #  PARAMETERS    : none
+ #
+ #######################################################################
+*/
+
+function saveComments() {
+
+	var com = $("textarea").val()
+	$.ajax({
+	    url: '/addComments',
+	    method: 'POST',
+	    data: {article_id:aid, comments: com},
+	    async: false,
+	}).done(function(r){
+
+			$("textarea").val("")
+			showArticles(aid,"")
+
+
+	   });
+ 
+}
+
+/*
+ #######################################################################
+ #
+ #  FUNCTION NAME : deleteComments
+ #  AUTHOR        : Maricel Louise Sumulong
+ #  DATE          : May 02, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : delete saved comments in the database
+ #  PARAMETERS    : article id
+ #
+ #######################################################################
+*/
+
+function deleteComment(id) {
+
+	$.ajax({
+	    url: '/deleteComments/' + id,
+	    method: 'DELETE',
+	    async: false,
+	}).done(function(r){
+
+			if (r == "1")
+				showArticles(aid,"")
+
+	   });
+}
---
+++ server.js
@@ -0,0 +1,231 @@
+//Load required packages
+require("dotenv").config();
+var express = require("express")
+var mongo = require("mongojs")
+var request = require("request")
+var cheerio = require("cheerio")
+var axios = require("axios")
+var bodyParser = require('body-parser')
+var app = express();
+
+//PORT Flexibility Declaration
+var port = process.env.PORT || 3000
+
+//Initialize Express
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongo(databaseUrl, collections);
+db.on("error", function(error) {
+	console.log("Database Error:", error);
+});
+
+app.get("/", function(req,res) {
+	//res.render("pages/index")
+	res.redirect("/fetchArticle")
+})
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  
+  // Make a request via axios for the news section of `the onion`
+  axios.get("https://www.theonion.com/c/news-in-brief").then(function(response) {
+    // Load the html body from axios into cheerio
+    var $ = cheerio.load(response.data);
+    // For each element with a "title" class
+    $(".headline").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+
+      var title = $(element).children("a").children("div").text();
+      var link = $(element).children("a").attr("href");
+      var sum = $(element).next().next().children("p").text()
+
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.scrapedData.insert({
+          title: title,
+          link: link,
+          summary: sum
+        },
+        function(err, inserted) {
+          if (err) {
+	        	// Log the error if one is encountered during the query
+	       		res.json({error:err})
+          }
+          else {
+              	// Send a "Scrape Complete" message to the browser
+  				
+          }
+        });
+      }
+    });
+
+    res.redirect("/fetchArticle")
+
+  });
+
+});
+
+app.get("/fetchArticle",function(req,res) {
+
+	//find articles
+	db.scrapedData.find({},function(error, news){
+		if(error) {
+		  console.log(error)
+		} else {
+			var o = {news:news}
+			res.render("pages/index",o)
+	  	  }
+	});
+
+})
+
+app.get("/check/:id", function(req,res) {
+
+	//find articles by id
+	db.saved.findOne({
+	  article_id: req.params.id
+	}, function(error, oneArticle) {
+	  if (error) {
+	    res.send(error);
+	  }else {
+	    res.json(oneArticle);
+	  }
+	});
+
+})
+
+app.put("/save/:id", function(req,res){
+
+	db.saved.insert({
+		article_id : req.params.id
+	},
+    function(err, inserted) {
+      if (err) {
+        	// Log the error if one is encountered during the query
+       		res.json({error:err})
+      }
+      else {
+          	// Send a "Scrape Complete" message to the browser
+				
+      }
+    });
+
+    res.send("1")
+
+})
+
+app.get("/fetchSavedArticles",function(req,res) {
+
+	//find saved articles
+	db.saved.find({},function(error, news1){
+		if(error) {
+		  console.log(error)
+		} else {
+			//var o = {news:news}
+			//res.render("pages/save",o)
+			var newsId = []
+			var o
+			for (var t = 0; t < news1.length; t++) {
+				newsId.push(mongo.ObjectId(news1[t].article_id))
+			}
+			db.scrapedData.find({
+				"_id": {$in:newsId}
+			}, function(error, oneArticle) {
+			  
+			   		if (error) {
+			        	console.log(error);
+			      	} else {
+			        	o = {news:oneArticle}
+			        	//console.log(o)
+						res.render("pages/save",o)
+			      	  }
+
+			});	
+			
+	  	  }
+	});
+
+})
+
+app.delete("/delete/:id", function(req,res){
+
+	db.saved.remove({
+		article_id : req.params.id
+    },true,function(err, result) {
+    	if (result.deletedCount == 1) {
+    		res.send("1")
+    		db.comments.remove({
+    			"article_id": req.params.id
+    		},false)
+    	} else {
+    		res.send(err)
+    	  }
+    });
+
+})
+
+app.get("/fetchComments/:id",function(req,res) {
+
+	//find comments by article id
+	db.comments.find({
+	  article_id: req.params.id
+	}, function(error, oneArticle) {
+	  if (error) {
+	    res.send(error);
+	  }else {
+	    res.json(oneArticle);
+	  }
+	});
+
+
+})
+
+app.post("/addComments", function(req,res) {
+
+	db.comments.insert({
+          article_id : req.body.article_id,
+          comments : req.body.comments 
+    },
+    function(err, inserted) {
+      if (err) {
+        	// Log the error if one is encountered during the query
+       		res.json({error:err})
+      }
+      else {
+          	// Send a "Scrape Complete" message to the browser
+			res.json({message:"1"})
+      }
+    });
+
+})
+
+app.delete("/deleteComments/:id", function(req,res){
+
+	db.comments.remove({
+		"_id" : mongo.ObjectId(req.params.id)
+    },true,function(err, result) {
+    	if (result.deletedCount == 1) {
+    		res.send("1")
+    	} else {
+    		res.send(err)
+    	  }
+    });
+
+})
+
+//Listening to port
+app.listen(port, function() {
+  console.log("App running on port "+port+"!");
+});
